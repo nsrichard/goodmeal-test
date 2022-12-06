726c587ec95c5348b538e2b73f2cdeb5a4f3df2c
Richard Cuizara <rcuizara@develoop.net>
2022-12-06 10:46:18 -0400

    frontend project init: vuejs3 and tailwind

---
+++ apps/frontend/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Hello</h1>
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from "vue-router";
+</script>
+
+<style lang="scss" scoped></style>
