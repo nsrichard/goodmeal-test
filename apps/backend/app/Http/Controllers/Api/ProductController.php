44c3372740c9110ce1976af337ec7ac4752181ea
Richard Cuizara <rcuizara@develoop.net>
2022-12-07 09:37:00 -0400

    generate documentation

--- apps/backend/app/Http/Controllers/Api/ProductController.php
+++ apps/backend/app/Http/Controllers/Api/ProductController.php
@@ -12,44 +12,97 @@ use App\Models\Store;
 class ProductController extends Controller
 {
 
-    public function index(Request $request)
-    {
-        $store_id = (int) $request->input('store_id', -1);
-        $products = Product::where(function ($query) use ($store_id){
-            if ($store_id > -1){
-                $query->where('store_id', '=', $store_id);
-            }
-        })->with('category')->get();
-        return $this->response(StatusCode::HTTP_OK, 'OK', ['message' => 'products list', 'data' => $products]);
-    }
+	/** @OA\Get(path="/api/product", tags={"product"}, summary="product list",
+	 *     @OA\Parameter(name="store_id", in="path", required=false, @OA\Schema(type="integer") ),
+	 *     @OA\Response(response=200, description="HTTP_OK"),
+	 *     @OA\Response(response=401, description="HTTP_UNAUTHORIZED"),
+	 *     @OA\Response(response=500, description="HTTP_INTERNAL_SERVER_ERROR")
+	 * )*/
+	public function index(Request $request)
+	{
+		$store_id = (int) $request->input('store_id', -1);
+		$products = Product::where(function ($query) use ($store_id){
+			if ($store_id > -1){
+				$query->where('store_id', '=', $store_id);
+			}
+		})->with('category')->get();
+		return $this->response(StatusCode::HTTP_OK, 'OK', ['message' => 'products list', 'data' => $products]);
+	}
 
-    public function show(Product $product)
-    {
-        return $this->response(StatusCode::HTTP_OK, 'OK', ['message' => 'found data', 'data' => $product->load('category')]);
-    }
+	/** @OA\Get(path="/api/product/{id}", tags={"product"}, summary="show product",
+	 *     @OA\Parameter(name="id", in="path", required=true, @OA\Schema(type="integer") ),
+	 *     @OA\Response(response=200, description="HTTP_OK"),
+	 *     @OA\Response(response=404, description="HTTP_NOT_FOUND"),
+	 *     @OA\Response(response=401, description="HTTP_UNAUTHORIZED"),
+	 *     @OA\Response(response=500, description="HTTP_INTERNAL_SERVER_ERROR")
+	 * )*/
+	public function show(Product $product)
+	{
+		return $this->response(StatusCode::HTTP_OK, 'OK', ['message' => 'found data', 'data' => $product->load('category')]);
+	}
 
-    public function store(ProductRequest $request){
-        $product = new Product();
-        if ($product->fillAndSave($request->all()))
-            return $this->response(StatusCode::HTTP_CREATED, 'OK', ['message' => 'created successfully', 'data' => $product]);
-        else 
-            return $this->response(StatusCode::HTTP_ERROR_INTERNAL, 'KO', ['message' => 'Internal error']);
-    }
+	/** @OA\Post(path="/api/product", tags={"product"}, summary="add product",
+	 *     @OA\RequestBody( @OA\MediaType( mediaType="application/json", @OA\Schema(
+	 *       @OA\Property(property="store_id", type="integer"),
+	 *       @OA\Property(property="category_id", type="integer"),
+	 *       @OA\Property(property="name", type="string"),
+	 *       @OA\Property(property="description", type="string"),
+	 *       @OA\Property(property="image", type="string"),
+	 *       @OA\Property(property="price", type="string"),
+	 *       @OA\Property(property="discount", type="string"),
+	 *       @OA\Property(property="stock", type="double"),
+	 *       example={"store_id": 1, "category_id": 1, "name": "test name", "description": "description data", "image": "https://www...", "price":15.5, "discount": 20.0, "stock": 25}
+	 *     ))),
+	 *     @OA\Response(response=201, description="HTTP_CREATED"),
+	 *     @OA\Response(response=403, description="HTTP_FORBIDDEN"),
+	 *     @OA\Response(response=500, description="HTTP_INTERNAL_SERVER_ERROR")
+	 * )*/
+	public function store(ProductRequest $request){
+		$product = new Product();
+		if ($product->fillAndSave($request->all()))
+			return $this->response(StatusCode::HTTP_CREATED, 'OK', ['message' => 'created successfully', 'data' => $product]);
+		else 
+			return $this->response(StatusCode::HTTP_ERROR_INTERNAL, 'KO', ['message' => 'Internal error']);
+	}
 
-    public function update(ProductRequest $request, Product $product)
-    {
-        if ($product->fillAndSave($request->all()))
-            return $this->response(StatusCode::HTTP_OK, 'OK', ['message' => 'updated successfully', 'data' => $product]);
-        else 
-            return $this->response(StatusCode::HTTP_ERROR_INTERNAL, 'KO', ['message' => 'Internal error']);
-    }
+	/** @OA\Put(path="/api/product/{id}", tags={"product"}, summary="update product",
+	 *     @OA\RequestBody( @OA\MediaType( mediaType="application/json", @OA\Schema(
+	 *       @OA\Property(property="store_id", type="integer"),
+	 *       @OA\Property(property="category_id", type="integer"),
+	 *       @OA\Property(property="name", type="string"),
+	 *       @OA\Property(property="description", type="string"),
+	 *       @OA\Property(property="image", type="string"),
+	 *       @OA\Property(property="price", type="string"),
+	 *       @OA\Property(property="discount", type="string"),
+	 *       @OA\Property(property="stock", type="double"),
+	 *       example={"store_id": 1, "category_id": 1, "name": "test name", "description": "description data", "image": "https://www...", "price":15.5, "discount": 20.0, "stock": 25}
+	 *     ))),
+	 *     @OA\Response(response=201, description="HTTP_OK"),
+	 *     @OA\Response(response=404, description="HTTP_NOT_FOUND"),
+	 *     @OA\Response(response=403, description="HTTP_FORBIDDEN"),
+	 *     @OA\Response(response=500, description="HTTP_INTERNAL_SERVER_ERROR")
+	 * )*/
+	public function update(ProductRequest $request, Product $product)
+	{
+		if ($product->fillAndSave($request->all()))
+			return $this->response(StatusCode::HTTP_OK, 'OK', ['message' => 'updated successfully', 'data' => $product]);
+		else 
+			return $this->response(StatusCode::HTTP_ERROR_INTERNAL, 'KO', ['message' => 'Internal error']);
+	}
 
-    public function destroy(Product $product)
-    {
-        if ($product->delete()) 
-            return $this->response(StatusCode::HTTP_OK, 'OK', ['response' => 'deleted successfully']);
-        else 
-            return $this->response(StatusCode::HTTP_ERROR_INTERNAL, 'OK', ['response' => 'Internal error']);
-    }
+	/** @OA\Delete(path="/api/product/{id}", tags={"product"}, summary="delete product",
+	 *     @OA\Parameter(name="id", in="path", required=true, @OA\Schema(type="integer") ),
+	 *     @OA\Response(response=200, description="HTTP_OK"),
+	 *     @OA\Response(response=404, description="HTTP_NOT_FOUND"),
+	 *     @OA\Response(response=403, description="HTTP_FORBIDDEN"),
+	 *     @OA\Response(response=500, description="HTTP_INTERNAL_SERVER_ERROR")
+	 * )*/
+	public function destroy(Product $product)
+	{
+		if ($product->delete()) 
+			return $this->response(StatusCode::HTTP_OK, 'OK', ['response' => 'deleted successfully']);
+		else 
+			return $this->response(StatusCode::HTTP_ERROR_INTERNAL, 'OK', ['response' => 'Internal error']);
+	}
 
 }
--- apps/backend/app/Http/Controllers/Api/StoreController.php
+++ apps/backend/app/Http/Controllers/Api/StoreController.php
@@ -11,45 +11,89 @@ use App\Models\Store;
 class StoreController extends Controller
 {
 
-    /**
-     * @OA\Get(
-     *     path="/api/store",
-     *     @OA\Response(response="200", description="stores list")
-     * )
-     */
-    public function index()
-    {
-        $stores = Store::get();
-        return $this->response(StatusCode::HTTP_OK, 'OK', ['message' => 'stores list', 'data' => $stores]);
-    }
+	/** @OA\Get(path="/api/store", tags={"store"}, summary="Store list",
+	 *     @OA\Response(response=200, description="HTTP_OK"),
+	 *     @OA\Response(response=401, description="HTTP_UNAUTHORIZED"),
+	 *     @OA\Response(response=500, description="HTTP_INTERNAL_SERVER_ERROR")
+	 * )*/
+	public function index()
+	{
+		$stores = Store::get();
+		return $this->response(StatusCode::HTTP_OK, 'OK', ['message' => 'stores list', 'data' => $stores]);
+	}
 
-    public function show(Store $store)
-    {
-        return $this->response(StatusCode::HTTP_OK, 'OK', ['message' => 'found data', 'data' => $store]);
-    }
+	/** @OA\Get(path="/api/store/{id}", tags={"store"}, summary="show store",
+	 *     @OA\Parameter(name="id", in="path", required=true, @OA\Schema(type="integer") ),
+	 *     @OA\Response(response=200, description="HTTP_OK"),
+	 *     @OA\Response(response=404, description="HTTP_NOT_FOUND"),
+	 *     @OA\Response(response=401, description="HTTP_UNAUTHORIZED"),
+	 *     @OA\Response(response=500, description="HTTP_INTERNAL_SERVER_ERROR")
+	 * )*/
+	public function show(Store $store)
+	{
+		return $this->response(StatusCode::HTTP_OK, 'OK', ['message' => 'found data', 'data' => $store]);
+	}
 
-    public function store(StoreRequest $request){
-        $store = new Store();
-        if ($store->fillAndSave($request->all()))
-            return $this->response(StatusCode::HTTP_CREATED, 'OK', ['message' => 'created successfully', 'data' => $store]);
-        else 
-            return $this->response(StatusCode::HTTP_ERROR_INTERNAL, 'KO', ['message' => 'Internal error']);
-    }
+	/** @OA\Post(path="/api/store", tags={"store"}, summary="add store",
+	 *     @OA\RequestBody( @OA\MediaType( mediaType="application/json", @OA\Schema(
+	 *       @OA\Property(property="name", type="string"),
+	 *       @OA\Property(property="about", type="string"),
+	 *       @OA\Property(property="logo", type="string"),
+	 *       @OA\Property(property="banner", type="string"),
+	 *       @OA\Property(property="service", type="string"),
+	 *       @OA\Property(property="lat", type="double"),
+	 *       @OA\Property(property="lng", type="double"),
+	 *       example={"name": "test name", "about": "about data", "logo": "https://www...", "banner":"https://www...", "service": "all", "lat": 79.58484981, "lng": 19.61854198}
+	 *     ))),
+	 *     @OA\Response(response=201, description="HTTP_CREATED"),
+	 *     @OA\Response(response=403, description="HTTP_FORBIDDEN"),
+	 *     @OA\Response(response=500, description="HTTP_INTERNAL_SERVER_ERROR")
+	 * )*/
+	public function store(StoreRequest $request){
+		$store = new Store();
+		if ($store->fillAndSave($request->all()))
+			return $this->response(StatusCode::HTTP_CREATED, 'OK', ['message' => 'created successfully', 'data' => $store]);
+		else 
+			return $this->response(StatusCode::HTTP_ERROR_INTERNAL, 'KO', ['message' => 'Internal error']);
+	}
 
-    public function update(StoreRequest $request, Store $store)
-    {
-        if ($store->fillAndSave($request->all()))
-            return $this->response(StatusCode::HTTP_OK, 'OK', ['message' => 'updated successfully', 'data' => $store]);
-        else 
-            return $this->response(StatusCode::HTTP_ERROR_INTERNAL, 'KO', ['message' => 'Internal error']);
-    }
+	/** @OA\Put(path="/api/store/{id}", tags={"store"}, summary="update store",
+	 *     @OA\RequestBody( @OA\MediaType( mediaType="application/json", @OA\Schema(
+	 *       @OA\Property(property="name", type="string"),
+	 *       @OA\Property(property="about", type="string"),
+	 *       @OA\Property(property="logo", type="string"),
+	 *       @OA\Property(property="banner", type="string"),
+	 *       @OA\Property(property="service", type="string"),
+	 *       @OA\Property(property="lat", type="double"),
+	 *       @OA\Property(property="lng", type="double"),
+	 *       example={"name": "test name", "about": "about data", "logo": "https://www...", "banner":"https://www...", "service": "all", "lat": 79.58484981, "lng": 19.61854198}
+	 *     ))),
+	 *     @OA\Response(response=201, description="HTTP_OK"),
+	 *     @OA\Response(response=404, description="HTTP_NOT_FOUND"),
+	 *     @OA\Response(response=403, description="HTTP_FORBIDDEN"),
+	 *     @OA\Response(response=500, description="HTTP_INTERNAL_SERVER_ERROR")
+	 * )*/
+	public function update(StoreRequest $request, Store $store)
+	{
+		if ($store->fillAndSave($request->all()))
+			return $this->response(StatusCode::HTTP_OK, 'OK', ['message' => 'updated successfully', 'data' => $store]);
+		else 
+			return $this->response(StatusCode::HTTP_ERROR_INTERNAL, 'KO', ['message' => 'Internal error']);
+	}
 
-    public function destroy(Store $store)
-    {
-        if ($store->delete()) 
-            return $this->response(StatusCode::HTTP_OK, 'OK', ['response' => 'deleted successfully']);
-        else 
-            return $this->response(StatusCode::HTTP_ERROR_INTERNAL, 'OK', ['response' => 'Internal error']);
-    }
+	/** @OA\Delete(path="/api/store/{id}", tags={"store"}, summary="delete store",
+	 *     @OA\Parameter(name="id", in="path", required=true, @OA\Schema(type="integer") ),
+	 *     @OA\Response(response=200, description="HTTP_OK"),
+	 *     @OA\Response(response=404, description="HTTP_NOT_FOUND"),
+	 *     @OA\Response(response=403, description="HTTP_FORBIDDEN"),
+	 *     @OA\Response(response=500, description="HTTP_INTERNAL_SERVER_ERROR")
+	 * )*/
+	public function destroy(Store $store)
+	{
+		if ($store->delete()) 
+			return $this->response(StatusCode::HTTP_OK, 'OK', ['response' => 'deleted successfully']);
+		else 
+			return $this->response(StatusCode::HTTP_ERROR_INTERNAL, 'OK', ['response' => 'Internal error']);
+	}
 
 }
